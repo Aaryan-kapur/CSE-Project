6f4ea8d0c12b3983bb8e1fe9f358ee87b64ce8d8
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-11-11 11:05:34 +0530

    meh

--- react/src/scripts/Test/Test.jsx
+++ react/src/scripts/Test/Test.jsx
@@ -3,6 +3,8 @@ import '../../styles/Test/Test.scss'
 import "../../styles/Test/Output.scss";
 import delay from 'delay'
 import img1 from '../../assets/1.svg'
+import Upper from "../Upper";
+
 import bsCustomFileInput from 'bs-custom-file-input'
 // import Output from "./Output";
 // import axios from 'axios'
@@ -17,9 +19,11 @@ class Test extends Component {
 		let fileInput = document.getElementById('inputGroupFile01')
 		let fileName = fileInput.files[0].name
 		let path_out = 'http://localhost:5000/'
-		// this.setState({})
-		// await delay(500)
+		this.state.l.length=0
+		await delay(1500)
 		let l = Array.from(this.state.l)
+		document.getElementById('data').style.visibility="visible";
+		document.getElementById('loading').style.visibility="hidden";
 		l.push(path_out + '1/' + fileName)
 		l.push(path_out + '2/' + fileName)
 		l.push(path_out + '3/' + fileName)
@@ -34,11 +38,14 @@ class Test extends Component {
 
 	componentDidMount() {
 		bsCustomFileInput.init()
+		document.getElementById('data').style.visibility="hidden";
+		document.getElementById('data').style.fontWeight=700;
 	}
 	state = {}
 	render() {
 		return (
 			<React.Fragment>
+				<Upper />
 				<div className="container">
 					<div className="div1 z-depth-3">
 						<div className="row">
@@ -103,11 +110,16 @@ class Test extends Component {
 						<div className="div10 z-depth-3">
 							<span className="heading " >Output</span>
 							<br />
-							<img className="imageOutput" src={this.state.l[0]} alt="" /><br/><br />
-							<img className="imageOutput" src={this.state.l[1]} alt="" /><br/><br />
-							<img className="imageOutput" src={this.state.l[2]} alt="" /><br/><br />
-							<img className="imageOutput" id ="1" src={this.state.l[3]} alt="" />
+<p id="loading">Loading...</p>							
+							<div className="data" id="data">
+							<br/>
+							<img className="imageOutput" src={this.state.l[0]} alt="" />Type A<br/><br />
+							<img className="imageOutput" src={this.state.l[1]} alt="" />Type B<br/><br />
+							<img className="imageOutput" src={this.state.l[2]} alt="" />Type C<br/><br />
+							<img className="imageOutput" id ="1" src={this.state.l[3]} alt="" />Type D <br/><br/>
+							If the image is black then no defect exists. The white area is the segmented defect.
 							{/* <img className="imageOutput" src={this.state.l[0]} alt="" /> */}
+							</div>
 						</div>
 					</div>
 				</React.Fragment>
